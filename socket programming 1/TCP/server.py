d1ca937c92a254e806029be35261553cea4891a5
Jianliang Luo <42287494+jluo1996@users.noreply.github.com>
2021-05-24 19:14:13 -0700

    Add files via upload

---
+++ socket programming 1/TCP/server.py
@@ -0,0 +1,14 @@
+from socket import*
+
+serverPort = 40726
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)  #server begins listening for incoming TCP requests
+print 'The server is ready to receive'
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024)
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence)
+    connectionSocket.close()
